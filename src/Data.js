193619c86fc188632acc2167e08fd40ca0bc56a4
Raj kr. soni <72248764+bugRaj@users.noreply.github.com>
2022-03-23 20:35:12 +0530

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,14 @@
+// import { text } from '../wine.data'; // Relative path to your File
+
+
+function Data() {
+    return (
+      <div>
+      <p>Dn App</p>
+      <Data/>
+      </div>
+    );
+  }
+  
+  export default Data;
+  
